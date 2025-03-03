c6be0ea0bf7e738c6279904db6d5108ae7f7bf1a
the elf <umarabdullah2609@gmail.com>
2025-03-03 23:21:28 +0530

    Update crack_pdf.py

--- crack_pdf.py
+++ crack_pdf.py
@@ -1,1 +1,13 @@
+import pikepdf
+from termcolor import colored
 
+file = open("wordlist.txt")
+
+for password in file:
+    try:
+        with pikepdf.open("remote.pdf", password.strip()) as p:
+            print(colored("Password Found: {}".format(password), "green"))
+            break
+    except:
+        print(colored("Trying Password: {}".format(password), "red"), end="")
+        continue
